743e7fd69ccacb9689ffdcea4d81b5a5823628a7
Wally Faye <wally.faye@gmail.com>
2019-12-29 16:37:39 -0500

    serverless deploy

--- utils/worldTime/index.ts
+++ utils/worldTime/index.ts
@@ -1,6 +1,6 @@
 export default {
     getCurrentTime: async () => {
-        const response = await fetch('http://worldtimeapi.org/api/ip')
+        const response = await fetch('https://worldtimeapi.org/api/ip')
         const { datetime } = await response.json()
         return datetime
     }
